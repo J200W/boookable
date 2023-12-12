c1dfff33ebe3777f7b3c12106549c0efadfbef0d
J200W <94389835+J200W@users.noreply.github.com>
2023-12-12 12:31:48 +0100

    Update button size in presentation and svg

--- frontend/src/components/home/Delivery.js
+++ frontend/src/components/home/Delivery.js
@@ -32,13 +32,7 @@ function Delivery() {
                                 aria-pressed="true"
                             >
                                 <p>J'AI UNE LISTE DE MATÉRIEL.</p>
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="20"
-                                    height="20"
-                                >
-                                    <path d="M10 20A10 10 0 1 0 0 10a10 10 0 0 0 10 10zM8.711 4.3l5.7 5.766L8.7 15.711l-1.4-1.422 4.289-4.242-4.3-4.347z" />
-                                </svg>
+                                <i class="fa fa-chevron-circle-right" aria-hidden="true"></i>
                             </a>
                             <a
                                 href="/"
@@ -47,13 +41,7 @@ function Delivery() {
                                 aria-pressed="true"
                             >
                                 <p>JE NE SAIS PAS CE QU'IL ME FAUT.</p>
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="20"
-                                    height="20"
-                                >
-                                    <path d="M10 20A10 10 0 1 0 0 10a10 10 0 0 0 10 10zM8.711 4.3l5.7 5.766L8.7 15.711l-1.4-1.422 4.289-4.242-4.3-4.347z" />
-                                </svg>
+                                <i class="fa fa-chevron-circle-right" aria-hidden="true"></i>
                             </a>
                         </div>
                     </div>
